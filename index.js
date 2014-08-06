8cf72a95667e4b368dbc7a39c94bef9b9ffb75f4
Jesse Ditson <jesse.ditson@gmail.com>
2014-08-06 13:16:45 -0700

    update script to print usage and initial work on silent mode, add preinstall

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ var xctool = require('./lib/xctool');
 var terminalmenu = require('terminal-menu');
 var fs = require('fs');
 var path = require('path');
+var async = require('async');
 
 var args = process.argv.slice(2);
 var hasArg = function(regex){
   return regex.test(args.join(' '));
 }
+
+var printUsage = function(){
+  console.log('usage: xctool-node [OPTIONS]');
+  console.log('Options:');
+  console.log('    xctool-node [-h|--help]              Print this help.');
+  console.log('    xctool-node [-s|--silent]            Run one of the silent commands - this will not show a prompt, just run.');
+  console.log('    xctool-node [-S|--suite]             Runs test suites instead of specific tests between resets. If in console mode, will execute selected suite instead of selected test.');
+}
+
+if (hasArg(/(^|\b)(-h|--help)(\b|$)/)) {
+  // print usage
+  printUsage();
+  return;
+}
+
+var runSuite = hasArg(/(^|\b)(-S|--suite)(\b|$)/);
 
 var menuInfo = { width: 40, x: 4, y: 4 };
 var menu;
@@ -26,11 +43,32 @@ var getObjectWithDepth = function(depth,o){
   }
 }
 
+var runTests = function(args,callback){
+  //SomeTestTarget:SomeTestClass/testSomeMethod
+  var onlyArg = args.slice(0,2).join(':');
+  if(args.length == 3){
+    onlyArg += '/' + args[2];
+  } else if(args.length != 2) {
+    throw new Error('Bad number of arguments passed to runTests - must be 2 or 3, had',args);
+  }
+  var test = xctool('run-tests','-freshSimulator','-freshInstall','-only',onlyArg);
+  test.stdout.pipe(process.stdout);
+  test.stderr.pipe(process.stderr);
+  test.on('close',function(code,signal){
+    callback();
+  });
+}
+
 var menuSelected = function(label) {
   menu.reset();
   menu.close();
   menu = terminalmenu(menuInfo);
-  if (label == 'EXIT') {
+  if (label == 'HELP') {
+    menu.reset();
+    menu.close();
+    printUsage();
+    return;
+  } else if (label == 'EXIT') {
     menu.reset();
     menu.close();
     return;
@@ -43,15 +81,10 @@ var menuSelected = function(label) {
     menu.write('~~~~   '+label+'   ~~~~\n');
     menu.write('-------------------------\n');
     // menu.add('BACK');
-    var runSuite = hasArg(/(^|\b)(-S|--suite)(\b|$)/);
     if (currentObj[label] === true || (runSuite && currentDepth.length == 1)) {
       menu.reset();
       menu.close();
-      //SomeTestTarget:SomeTestClass/testSomeMethod
-      var onlyArg = currentDepth.join(':') + (runSuite ? ':' : '/') + label;
-      var test = xctool('run-tests','-freshSimulator','-freshInstall','-only',onlyArg);
-      test.stdout.pipe(process.stdout);
-      test.stderr.pipe(process.stderr);
+      runTests(currentDepth.concat(label));
       return;
     } else {
       Object.keys(currentObj[label] ? currentObj[label] : currentObj).forEach(function(test){
@@ -61,44 +94,65 @@ var menuSelected = function(label) {
       currentObj = currentObj[label];
     }
   }
+  menu.add('EXIT');
   menu.on('select',menuSelected);
   menu.createStream().pipe(process.stdout);
 }
 
-if(!hasArg(/(^|\b)(-s|--silent)(\b|$)/)){
-  // show the gui
-  var currentTests;
-  var m;
-  xctool('run-tests','-listTestsOnly',function(data){
-    var line = data.toString().trim();
-    if(m = line.match(/^run-test\s+([^\(]+)/)){
+var currentTests;
+var m;
+xctool('run-tests','-listTestsOnly',function(data){
+  var line = data.toString().trim();
+  var runTestRegEx = /run-test\s+([^\(]+)/;
+  console.log(line);
+  if(runTestRegEx.test(line)){
+    while(m = runTestRegEx.exec(line)){
       var testsName = m[1].replace(/\.(o|x)ctest/i,'').trim();
       tests[testsName] = {};
       currentTests = testsName;
-    } else if(currentTests){
-      var testRegEx = /~ -\[([^\s]+)\s*([^\]]+)?\]/g;
-      while(m = testRegEx.exec(line)){
-        var suite = m[1];
-        tests[currentTests][suite] = tests[currentTests][suite] || {};
-        tests[currentTests][suite][m[2]] = true;
-      }
     }
-  },function(){
-    menu = terminalmenu(menuInfo);
-    currentObj = tests;
-    menu.reset();
-    menu.write('~~~~   XCODE TESTS   ~~~~\n');
-    menu.write('-------------------------\n');
-    Object.keys(tests).forEach(function(test){
-      menu.add(test);
+  } else if(currentTests){
+    var testRegEx = /~ -\[([^\s]+)\s*([^\]]+)?\]/g;
+    while(m = testRegEx.exec(line)){
+      var suite = m[1];
+      tests[currentTests][suite] = tests[currentTests][suite] || {};
+      tests[currentTests][suite][m[2]] = true;
+    }
+  }
+},function(){
+  if(!hasArg(/(^|\b)(-s|--silent)(\b|$)/)){
+    // show the gui
+      menu = terminalmenu(menuInfo);
+      currentObj = tests;
+      menu.reset();
+      menu.write('~~~~   XCODE TESTS   ~~~~\n');
+      menu.write('-------------------------\n');
+      menu.add('HELP');
+      menu.add('EXIT');
+      Object.keys(tests).forEach(function(test){
+        menu.add(test);
+      });
+      menu.on('select',menuSelected);
+      menu.createStream().pipe(process.stdout);
+  } else {
+    // cli interface
+    var runnables = [];
+    Object.keys(tests).forEach(function(target){
+      Object.keys(target).forEach(function(suite){
+        if (runSuite) {
+          runnables.push([target,suite]);
+        } else {
+          Object.keys(suite).forEach(function(test){
+            runnables.push([target,suite,test]);
+          });
+        }
+      });
     });
-    menu.on('select',menuSelected);
-    menu.createStream().pipe(process.stdout);
-  });
-} else if (!args.length || hasArg(/(^|\b)(-h|--help)(\b|$)/)) {
-  // print usage
-  // TODO
-} else {
-  // cli interface
-  // TODO
-}
+    return console.log(runnables);
+    async.series(runnables.map(function(arr){
+      return runTests.bind(null,arr);
+    }),function(){
+      console.log('done.');
+    });
+  }
+});
